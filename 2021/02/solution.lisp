313c825eae4313669f71a584ff1f9a87384bf810
Simon Kindström <simon.kindstrom@gmail.com>
2022-12-03 10:14:50 +0000

    2021 day 2 part 2

--- 2021/02/solution.lisp
+++ 2021/02/solution.lisp
@@ -10,8 +10,24 @@
 		   (t (format t "no matches"))))
 	finally (return (* horizontal depth))))
 
+(defun solution-2 ()
+  (loop for entry in (read-file)
+	with horizontal = 0
+	with depth = 0
+	with aim = 0
+	do (let ((action (car entry))
+		 (value (cdr entry)))
+	     (cond ((string= action "forward") (progn
+						 (setf horizontal (+ horizontal value))
+						 (setf depth (+ depth (* aim value)))))
+		   ((string= action "up") (setf aim (- aim value)))
+		   ((string= action "down") (setf aim (+ aim value)))
+		   (t (format t "no matches"))))
+	finally (return (* horizontal depth))))
+
+
 (defun read-file ()
   (mapcar #'(lambda (line) (let ((splits (uiop:split-string line)))
 			     (cons (elt splits 0)
 				   (parse-integer (elt splits 1)))))
-	      (uiop:read-file-lines "02/input.txt")))
+	      (uiop:read-file-lines "2021/02/input.txt")))
