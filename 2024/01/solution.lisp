38516edd766b59df92a7b8a216c8dadacf7016c2
Simon Kindström <simon.kindstrom@gmail.com>
2024-12-01 10:15:48 -0500

    2024 day 01 part 02

--- 2024/01/solution.lisp
+++ 2024/01/solution.lisp
@@ -3,13 +3,35 @@
 (defun solution-1 ()
   (reduce #'+
           (mapcar #'(lambda (pair) (abs (- (first pair)
-                                   (second pair))))
-          (apply #'mapcar
-                 #'list
-                 (mapcar #'(lambda (col)
-                             (sort col #'<))
-                         (merge-pairs (read-file)))))
+                                           (second pair))))
+                  (apply #'mapcar
+                         #'list
+                         (mapcar #'(lambda (col)
+                                     (sort col #'<))
+                                 (merge-pairs (read-file)))))
           :initial-value 0))
+
+(defun solution-2 ()
+  (destructuring-bind (first second) (merge-pairs (read-file))
+    (let ((occurences (occurence-map second)))
+      (reduce #'(lambda (sum val)
+                  (+ sum
+                     (* val
+                        (gethash val occurences 0))))
+              first
+              :initial-value 0))))
+
+(defun occurence-map (l)
+  (labels ((iter (l ht)
+             (if l
+                 (progn
+                   (setf (gethash (car l) ht)
+                         (1+ (or (gethash (car l) ht)
+                                 0)))
+                   (iter (cdr l)
+                         ht))
+                 ht)))
+    (iter l (make-hash-table))))
 
 (defun merge-pairs (pairs)
   (labels ((iter (pairs x y)
