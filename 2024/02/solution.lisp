e0b170ffc0d3fd1439a3f5eb53d1c2dbdb44af2b
Simon Kindström <simon.kindstrom@gmail.com>
2024-12-03 21:47:42 -0500

    2024 day 02 part 1, but recursive

--- 2024/02/solution.lisp
+++ 2024/02/solution.lisp
@@ -1,6 +1,21 @@
 (defparameter *file* (pathname "input.txt"))
 (defun solution-1 ()
-  (count-if #'is-safe (read-reports)))
+  (count-if #'is-safe-recur (read-reports)))
+
+(defun is-safe-recur (report)
+  (labels ((iter (report direction)
+             (destructuring-bind (a b &rest rest) report
+               (and (case direction
+                      (:up (< (car report) (cadr report)))
+                      (:down (> (car report) (cadr report))))
+                    (<= 1 (abs (- a b)) 3)
+                    (if (null rest)
+                        t
+                        (iter (cons b rest) direction)
+                        )))))
+    (iter report (if (< (car report) (cadr report))
+                     :up
+                     :down))))
 
 (defun is-safe (report)
   (and (is-increasing-or-decreasing report)
@@ -20,9 +35,9 @@
 (defun is-adjacent-levels-safe (report)
   (every (lambda (val) (<= 1 val 3))
          (mapcar (lambda (a b)
-            (abs (- a b)))
-          report
-          (cdr report))))
+                   (abs (- a b)))
+                 report
+                 (cdr report))))
 
 (defun read-reports ()
   (mapcar #'(lambda (line)
