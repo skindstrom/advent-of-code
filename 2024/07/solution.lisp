04df8087a2380fd6927792d3b1bad77ff93e23ba
Simon Kindström <simon.kindstrom@gmail.com>
2024-12-08 16:09:58 -0500

    2024 day 07 part 01

---
+++ 2024/07/solution.lisp
@@ -0,0 +1,39 @@
+(use-package :alexandria)
+(defparameter *file* (pathname "input.txt"))
+
+(defun solution-1 ()
+  (reduce (lambda (sum calibration)
+            (if (valid-calibration-p calibration)
+                (+ sum (result calibration))
+                sum))
+          (parse-file)
+          :initial-value 0))
+
+(defun valid-calibration-p (calibration)
+  "Returns t if the calibration is valid, nil otherwise"
+  (labels ((iter (curr parameters)
+             (if (null parameters)
+                 (= (result calibration) curr)
+                 (or (iter (+ curr
+                              (car parameters))
+                           (cdr parameters))
+                     (iter (* curr
+                              (car parameters))
+                           (cdr parameters))))))
+    (let ((parameters (parameters calibration)))
+      (iter (car parameters)
+            (cdr parameters)))))
+
+(defun result (calibration)
+  (car line))
+
+(defun parameters (calibration)
+  (cdr line))
+
+(defun parse-file ()
+  (mapcar (lambda (line)
+            (remove-if #'null
+                       (mapcar (lambda (val)
+                                 (parse-integer val :junk-allowed t))
+                               (uiop:split-string line :separator '(#\space #\:)))))
+          (uiop:read-file-lines *file*)))
