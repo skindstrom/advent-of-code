c1fb03b6237d70f833677b8deec15b33fe0fc544
Simon Kindström <simon.kindstrom@gmail.com>
2024-12-08 14:12:46 -0500

    2024 day 05 part 02, not working

--- 2024/05/solution.lisp
+++ 2024/05/solution.lisp
@@ -11,6 +11,15 @@
               updates
               :initial-value 0)))
 
+(defun solution-2 ()
+  (destructuring-bind (rules updates) (parse-file)
+      (reduce (lambda (acc update)
+                (if (not (valid-update-p update rules))
+                    (+ acc (middle (fix-update update rules)))
+                    acc))
+              updates
+              :initial-value 0)))
+
 (defun valid-update-p (update rules)
   (labels ((iter (seen update)
              "Returns the seen updates, or nil if invalid"
@@ -21,6 +30,26 @@
                                  invalid)
                      (iter (cons (car update) seen)
                            (cdr update)))))))
+    (iter nil update)))
+
+(defun fix-update (update rules)
+  (labels ((iter (seen update)
+             "Returns the seen updates, or nil if invalid"
+             (if (null update)
+                 seen
+                 (iter (swap (cons (car update)
+                                   seen))
+                       (cdr update))))
+           (swap (seen)
+             (let ((invalid (gethash (car seen) rules)))
+               (let ((pos (position-if (lambda (s)
+                                         (member s invalid))
+                                       seen
+                                       :from-end t)))
+                 (when pos
+                   (rotatef (car seen)
+                            (elt seen pos)))
+                 seen))))
     (iter nil update)))
 
 (defun middle (seq)
